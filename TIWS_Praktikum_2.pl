efc30b09a92627f17343b69a587771a28778865e
VShader <t.weitz66@web.de>
2013-11-04 18:09:56 +0100

    add some relations in 1b) and 2c)

--- TIWS_Praktikum_2.pl
+++ TIWS_Praktikum_2.pl
@@ -11,11 +11,29 @@ liste(list(A,Xs)) :- liste(Xs).
 %anz(list(A,Xs),s(N)) : N ist die Anzahl der Listenelemente der Liste list(A,Xs), wobei A ein Datenelement und Xs eine Teilliste ist.
 anz(nil,o).
 anz(list(A,Xs),s(N)) :- liste(Xs), anz(Xs,N).
+app(nil,Xs,Xs).
+app(list(X,X1s),Ys,list(X,X2s)):- app(X1s,Ys,X2s).
+präfix(Xs,Ys):-app(Xs,H1s,Ys).
+%infix(Xs,Ys):- app(Xs,Ys,Ys).
+postfix(Xs,Ys):-app(H1s,Xs,Ys).
+memb(X,list(X,Ys)) :-  memb(X,Ys).
+
 
 
 %Aufgabe 2
-%a) Ein Binärbaum hat ein Datenelement X einen linken Nachfolger Ls und einen rechten Rs.
+%a) Ein Binärbaum hat ein Datenelement X einen linken Nachfolger Lb und einen rechten Rb.
+
 %b)
-%binbaum(baum(X,Ls,Rs)) : binbaum ist ein baum wenn A ein Datenelement und Ls und Rs Teilbäume sind.
+%binbaum(baum(X,Lb,Rb)) : binbaum ist ein baum wenn A ein Datenelement und Lb und Rb Teilbäume sind.
 binbaum(nil).
-binbaum(baum(X,Ls,Rs)) :- binbaum(Ls), binbaum(Rs).
+binbaum(baum(X,Lb,Rb)) :- binbaum(Lb), binbaum(Rb).
+
+%c)
+root(baum(Y,Lb,Rb),Y).
+left(baum(X,Lb,Rb),Lb).
+right(baum(X,Lb,Rb),Rb).
+construct(X,Lb,Rb,baum(X,Lb,Rb)).
+knotenanz(nil,o).
+knotenanz(baum(X,Lb,Rb),s(N)) :- knotenanz(Lb,H1),knotenanz(Rb,H2),add(H1,H2,N).
+add(o,Y,Y).
+add(s(X),Y,s(E)):- add(X,Y,E).
