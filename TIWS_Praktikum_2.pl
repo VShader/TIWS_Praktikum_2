ac4081227909372a4e3bcb2b7ccf34c026aef4f1
VShader <t.weitz66@web.de>
2013-11-21 22:29:50 +0100

    add revers

--- TIWS_Praktikum_2.pl
+++ TIWS_Praktikum_2.pl
@@ -15,13 +15,23 @@ anz(list(A,Xs),s(N)) :- liste(Xs), anz(Xs,N).
 app(nil,Xs,Xs).
 app(list(X,X1s),Ys,list(X,X2s)):- app(X1s,Ys,X2s).
 
-präfix(Xs,Ys) :- app(Xs,H1s,Ys), H1s==nil.
+präfix(Xs,Ys) :- app(Xs,H1s,Ys).%, H1s==nil.
 
 infix(Xs,Ys) :- präfix(Xs,Ys).
 infix(Xs,list(X,Ys)) :- infix(Xs,Ys).%präfix(Xs,Ys).
 
+infix2(Xs,Ys) :- app(_,Xs,Hs), app(Hs,_,Ys).
+
 postfix(Xs,Ys):-app(H1s,Xs,Ys).
-memb(X,list(X,Ys)) :-  memb(X,Ys).
+memb(X,list(X,Ys)).
+memb(X,list(_,Ys)) :-  memb(X,Ys).
+
+lastelement(X,list(X,nil)).
+lastelement(X,list(X,Ys)) :- lastelement(X,Ys).
+
+%revers(Xs,Ys) : Ys ist Xs gespiegelt
+revers(nil,nil).
+revers(list(X,Xs),Ys) :- revers(Xs,Hs), app(Hs,list(X,nil),Ys).
 
 
 
@@ -30,13 +40,14 @@ memb(X,list(X,Ys)) :-  memb(X,Ys).
 
 %b)
 %binbaum(baum(X,Lb,Rb)) : binbaum ist ein baum wenn A ein Datenelement und Lb und Rb Teilbäume sind.
+
 binbaum(nil).
 binbaum(baum(X,Lb,Rb)) :- binbaum(Lb), binbaum(Rb).
 
 %c)
-root(baum(Y,Lb,Rb),Y).
-left(baum(X,Lb,Rb),Lb).
-right(baum(X,Lb,Rb),Rb).
+root(baum(Y,Lb,Rb),Y) :- binbaum(Lb), binbaum(Rb).
+left(baum(X,Lb,Rb),Lb) :- binbaum(Lb), binbaum(Rb).
+right(baum(X,Lb,Rb),Rb) :- binbaum(Rb).
 construct(X,Lb,Rb,baum(X,Lb,Rb)).
 knotenanz(nil,o).
 knotenanz(baum(X,Lb,Rb),s(N)) :- knotenanz(Lb,H1),knotenanz(Rb,H2),add(H1,H2,N).
